bf3059fbe224e5959817014f571585f9c8cf6672
Ardhra36 <76932837+Ardhra36@users.noreply.github.com>
2022-03-04 10:14:16 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+from wsgiref import simple_server
+from flask import Flask, request, render_template
+# from flask import Response
+import os
+from flask_cors import CORS, cross_origin
+from Data_prediction_process.DataPredictionProcessor import PredictionProcess
+
+
+
+app = Flask(__name__)
+CORS(app)  # what does it do?
+
+@app.route("/", methods=['GET'])
+@cross_origin()
+def home():
+    return render_template('indexnew.html')
+
+@app.route("/", methods=['POST'])
+@cross_origin()
+def predictRouteClient():
+    try:
+        data = {}
+        data['title'] = request.form['title']
+        data['location'] = request.form['location']
+        data['department'] = request.form['department']
+        data['salary_range'] = request.form['salary_range']
+        data['company_profile'] = request.form['company_profile']
+        description = request.form['description']
+        requirements = request.form['requirements']
+        benefits = request.form['benefits']
+
+        predictor = PredictionProcess(data)
+        predictor.find_probabilities()
+        prediction = predictor.get_prediction()
+
+        return render_template('indexnew.html', prediction=prediction)
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    app.run()
